169c0ee4b8bfce85179c32deb33d63e98445fd0d
aciukurescu <andrewciukurescu@gmail.com>
2018-01-19 13:41:58 -0500

    pushed over hexclock project

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function displayClock(){
+var d = new Date();
+var h = d.getHours();
+var m = d.getMinutes();
+var s = d.getSeconds();
+
+	if (h<=9){
+		h = '0'+h;
+	} if (m<=9){ 
+		m = '0'+m;
+	} if (s<=9) {
+		s = '0'+s;
+	}	
+	var color = '#'+h+m+s;
+
+document.body.style.background = color;
+
+document.getElementById("hex").innerHTML = color;
+
+setTimeout(displayClock, 1000);
+}
+
+displayClock();
