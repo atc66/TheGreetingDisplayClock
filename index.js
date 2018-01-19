a3ba7447e2691e22daad0d18d58c1fec2c419c20
aciukurescu <andrewciukurescu@gmail.com>
2018-01-19 14:47:05 -0500

    added img directory

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 function displayClock(){
+var hex = document.getElementById('hex')
 var d = new Date();
 var h = d.getHours();
 var m = d.getMinutes();
 var s = d.getSeconds();
 var greet = document.getElementById('greeting')
+var image = document.getElementsByClassName('image')
 
 	if (h<=9){
 		h = '0'+h;
@@ -17,10 +19,12 @@ var greet = document.getElementById('greeting')
 
 document.body.style.background = color;
 
-document.getElementById("hex").innerHTML = h + ':' + m + ':' +s;
+hex.innerHTML = h + ':' + m + ':' +s;
 
 if (h>=12 && h<=17){
 	greet.innerHTML="GOOD AFTERNOON!"
+	// document.body.style.backgroundImage = "/img/afternoon.jpg";
+	// image.backgroundImage=
 } else if (h>=17 && h<=20){
 	greet.innerHTML="GOOD EVENING!"
 } else if (h>=20 && h<=6){
